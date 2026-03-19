b27ba94c5c5ca27aa278dc18e47e4aa7a08b1052
nicolasgonzalez007 <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-03-19 19:25:28 +0100

    Add files via upload
    LA CAPTURA ES LA PRUEBA DE QUE FUNCIONA

---
+++ alumnos.sql
@@ -0,0 +1,8 @@
+INSERT INTO Alumnos (idAlumno,nombre)
+VALUES 
+    ('01','Nicolás'),
+    ('05','Angelo'),
+    ('06','Manuel'),
+    ('07','Victor'),
+    ('10','Luis');
+
---
+++ lista_alumnos.php
@@ -0,0 +1,32 @@
+<?php
+define('SERVIDOR', 'localhost');
+  define('USUARIO', 'root');
+  define('PASSWORD', ''); 
+  define('BBDD', 'prueba_sql');
+  // Función para conectar (se mantiene igual)
+  function conectar(){
+    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    $conexion->set_charset("utf8"); 
+    return $conexion;
+  }
+  
+  function mostrar_alumnos(){ 
+    $conexion = conectar();  
+    
+    $sql = "SELECT idAlumno, nombre FROM Alumnos ORDER BY idAlumno ASC";
+    $resultado = $conexion->query($sql);	
+    
+    // Obtenemos el número total de filas
+    $total_filas = $resultado->num_rows;
+
+    //Bucle 'for' para mostrar
+    for ($i = 0; $i < $total_filas; $i++) {
+        $fila = $resultado->fetch_array();
+        echo '<p>';
+        echo 'ID: ' . $fila["idAlumno"] . '- Alumno: ' . $fila["nombre"]; 
+        echo '</p>';
+    }
+    $conexion->close();
+  }
+  mostrar_alumnos();
+?>
