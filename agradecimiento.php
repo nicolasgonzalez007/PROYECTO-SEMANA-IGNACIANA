dc4af7f57761e0fe347647da4d1e4a09dee12704
nicolasgonzalez007 <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 13:13:19 +0100

    Update agradecimiento.php

--- agradecimiento.php
+++ agradecimiento.php
@@ -1,40 +1,61 @@
+<?php
+    define('SERVIDOR', 'localhost');
+    define('USUARIO', 'root');
+    define('PASSWORD', ''); 
+    define('BBDD', 'prueba_sql');
+    function conectar(){
+    $conexion = new mysqli(SERVIDOR, USUARIO, PASSWORD, BBDD);
+    $conexion->set_charset("utf8"); 
+    return $conexion;
+  }
+  function mostrar_alumnos(){ 
+    $conexion = conectar();  
+    
+    $sql = "SELECT idAlumno, nombre FROM Alumnos ";
+    $resultado = $conexion->query($sql);	
+    
+    // Obtenemos el número total de filas
+    $total_filas = $resultado->num_rows;
+
+   
+   while($fila=$resultado->fetch_array())
+    {
+      echo <p>
+      echo 'ID: ' . $fila["idAlumno"] . '- Alumno: ' . $fila["nombre"]; 
+      echo '</p>';
+    }
+    $conexion->close();
+  }
+?>
+
 <!DOCTYPE html>
 <head>
-    <meta charset="UTF-8">
-    <title>AGRADECIMIENTO</title>
-    <link rel="stylesheet" href="estilo.css">
+    <meta charset="UTF-8">
+    <title>AGRADECIMIENTO</title>
+    <link rel="stylesheet" href="estilo.css">
 </head>
 <body class="agradecimiento">
-    <div class="titulo_pag2">
-        <h2>AGRADECE <span>EN</span> COMPAÑIA</h2>
-        <img src="jesuitas.png">
-    </div>
-    <nav>
-        <a href="#">Agradecer</a>
-        <a href="#">Recibir</a>
-        <a href="#">Cerrar sesión</a>
-    </nav>   
-    <div class="agradecer">
-        <form action="#" method="post">
-            <label for="nombres">PARA</label>
-            <select id="nombres" name="nombres">
-                <?php
-
-                //Aqui iria un while de 1 hasta el nº de Alumnos registrados
-                <option value="1">Nombre 1</option>// El valor del value seria el idAlumno , y lo que va dentro del option seria el nombre del alumno o el pseudonimo de jesuita
-                
-
+    <div class="titulo_pag2">
+        <h2>AGRADECE <span>EN</span> COMPAÑIA</h2>
+        <img src="jesuitas.png">
+    </div>
+    <nav>
+        <a href="#">Agradecer</a>
+        <a href="#">Recibir</a>
+        <a href="#">Cerrar sesión</a>
+    </nav>   
+    <div class="agradecer">
+        <form action="#" method="post">
+            <label for="nombres">PARA</label>
+            <select id="nombres" name="nombres">
+                <?php
+                   mostrar_alumnos();   
                 ?>
-
-                <!--<option value="0" disabled selected>Selecciona un nombre</option>-->
-                <!--<option value="2">Nombre 2</option>
-                <option value="3">Nombre 3</option>-->
-            </select>
-            <label for="mensaje">QUIERO AGRADECERTE</label>
-            <textarea id="mensaje"name="mensaje" placeholder="Escribe aqui tu mensaje..."></textarea>
-            <input type="submit" value="AGRADECER">
-        </form>
-        
-    </div>
+            </select>
+            <label for="mensaje">QUIERO AGRADECERTE</label>
+            <textarea id="mensaje"name="mensaje" placeholder="Escribe aqui tu mensaje..."></textarea>
+            <input type="submit" value="AGRADECER">
+        </form>
+    </div>
 </body>
-</html> quiero poner una imagen al lado del titulo
+</html>
