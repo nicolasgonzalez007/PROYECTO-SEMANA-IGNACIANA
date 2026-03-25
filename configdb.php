85681bddb4de6c0606286b2c5e156a4fd0957d94
nicolasgonzalez007 <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-03-25 13:03:53 +0100

    Create configdb.php

---
+++ configdb.php
@@ -0,0 +1,6 @@
+<?php
+    define('SERVIDOR', '13.1daw.esvirgua.com');
+    define('USUARIO', 'user1daw_13');
+    define('PASSWORD', 'OOhWU9g&C+dM8)vo'); 
+    define('BBDD', 'user1daw_BD1-13');
+    ?>
