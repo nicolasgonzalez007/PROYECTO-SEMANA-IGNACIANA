8ca086c9066cd174c49d91a1777187141191dca3
nicolasgonzalez007 <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-04-21 13:12:32 +0200

    Update alumnos.sql

--- alumnos.sql
+++ alumnos.sql
@@ -1,19 +1,19 @@
 CREATE TABLE alumnos (
 	equipo char(2) NOT NULL PRIMARY KEY,
 	nombre varchar(40) NOT NULL,
-	usuario varchar(20) NOT NULL UNIQUE, -- Nombre con el que se inicia sesión. Sin espacios
+	usuario varchar(20) NOT NULL UNIQUE, 
 	password varchar(20) NOT NULL,
 	nombreJesuita varchar(100) NOT NULL,
 	infoJesuita varchar(250)NOT NULL,
-	web varchar(30) NOT NULL UNIQUE -- Nombre de la carpeta, que debe tener un archivo index.php, más el css y la imagen del jesuita.	
+	web varchar(30) NOT NULL UNIQUE 	
 );
 
 CREATE TABLE agradecimientos (
-    idAgradecimiento smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    idAgradecimiento smallint  NOT NULL PRIMARY KEY IDENTITY,
 	mensaje varchar(300) NOT NULL,
 	idEmisor char(2) NOT NULL,
 	idReceptor char(2) NOT NULL,
-	fecha_hora timestamp default NOW(),
+	fecha_hora datetime default GETDATE(),
 	CONSTRAINT CK_diferentes CHECK (idEmisor!=idReceptor ),
 	CONSTRAINT FK_emisor FOREIGN KEY (idEmisor) REFERENCES alumnos (equipo),
 	CONSTRAINT FK_receptor FOREIGN KEY (idReceptor) REFERENCES alumnos (equipo)
