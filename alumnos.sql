4dcabb8d6a1218085ff9112efba3d67c17f8cd71
nicolasgonzalez007 <ngonzalezmorera.guadalupe@alumnado.fundacionloyola.net>
2026-04-21 12:37:32 +0200

    Update alumnos.sql

--- alumnos.sql
+++ alumnos.sql
@@ -1,27 +1,21 @@
-
-CREATE TABLE Alumnos (
-    puesto CHAR(2) not null ,
-    nombre VARCHAR(100) NOT NULL, --Nombre carpeta .SIN ESPACIOS
-    usuario varchar(20) NOT NULL,
-    password  varchar (20)not null,
-    nombreJesuita VARCHAR(100)not null,
-    infoJesuita TEXT not null,
-    web varchar(250) not null, --
-    PRIMARY KEY (puesto)
+CREATE TABLE alumnos (
+	equipo char(2) NOT NULL PRIMARY KEY,
+	nombre varchar(40) NOT NULL,
+	usuario varchar(20) NOT NULL UNIQUE, -- Nombre con el que se inicia sesión. Sin espacios
+	password varchar(20) NOT NULL,
+	nombreJesuita varchar(100) NOT NULL,
+	infoJesuita varchar(250)NOT NULL,
+	web varchar(30) NOT NULL UNIQUE -- Nombre de la carpeta, que debe tener un archivo index.php, más el css y la imagen del jesuita.	
 );
 
-CREATE TABLE Mensaje (
-    idMensaje INT AUTO_INCREMENT,
-    texto TEXT NOT NULL,
-    idAlumno_emisor char (2),
-    idAlumno_receptor char(2),
-    PRIMARY KEY (idMensaje),
-    CONSTRAINT fk_emisor FOREIGN KEY (idAlumno_emisor) REFERENCES ALUMNOS(puesto), 
-    CONSTRAINT fk_receptor FOREIGN KEY (idAlumno_receptor) REFERENCES ALUMNOS(puesto)       
+CREATE TABLE agradecimientos (
+    idAgradecimiento smallint unsigned NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	mensaje varchar(300) NOT NULL,
+	idEmisor char(2) NOT NULL,
+	idReceptor char(2) NOT NULL,
+	fecha_hora timestamp default NOW(),
+	CONSTRAINT CK_diferentes CHECK (idEmisor!=idReceptor ),
+	CONSTRAINT FK_emisor FOREIGN KEY (idEmisor) REFERENCES alumnos (equipo),
+	CONSTRAINT FK_receptor FOREIGN KEY (idReceptor) REFERENCES alumnos (equipo)
+
 );
-
-
-
-
-
-
